ccaabb6a3684f58c7e64a9244feafab2d961e0f4
sharafhu <115363443+sharafhu@users.noreply.github.com>
2022-11-06 14:42:29 +0300

    first commit

---
+++ files/editor/files/config.js
@@ -0,0 +1,109 @@
+var readerConfig = {
+  "pageConfig": {
+    "pageWidth": 720,
+    "pageHeight": 1040,
+    "BGMInfo": [],
+    "watermarkInfo": {
+      "isShowMark": false,
+      "watermarks": []
+    }
+  },
+  "pages": [
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    },
+    {
+      "insertPage": null,
+      "insertPageLink": []
+    }
+  ]
+};
